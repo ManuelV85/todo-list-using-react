7d4e6e3d7652b80d7d18110a0f544bf6df93b5b4
ManuelV85 <manuel.villater@gmail.com>
2023-02-05 18:56:40 +0000

    html ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,24 +5,55 @@ import { useEffect, useState } from "react";
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	const[task, setTask] = useState([])
-	const newTask = (e) => {
-		if(e.key === 'Enter'){
-			setTask (prev =>[...prev, e.target.value])
-			
-		}
-	}
-	return (
-    <div id="container" class="container container fluid">
-    <h1>TodoList</h1>
-    <input id="input" type="text" placeholder="Add New Task ..." onKeyDown={e => newTask(e)} />
-  {task.map((task) => {
-  return <h1>{task}</h1>
-  })
+  const [task, setTask] = useState([])
+  const newTask = (e) =>{
+    if (e.key === 'Enter'){
+      setTask(prev =>[...prev, e.target.value])
+      
+    }
+
   }
-  </div>
-);
-};
+
+  return (
+    <div className="container">
+      <h1>TODO LIST</h1>
+      <h3>Add New Task</h3>
+      <br />
+      
+      <input type="text" placeholder="Add New Task..." onKeyDown={e=>newTask(e)}/>
+      {task.map((task) =>{return <h3>{task}</h3>})}
+      <br />
+      <br />
+
+
+      <h2>TODO</h2>
+      <ul id="incompleteTask">
+        <li><input type="checkbox" /><label>Pay Bills</label><input type="text" /><button className="edit">Edit</button>
+        <button className="delete">Delete</button></li>
+        <li><input type="checkbox" /><label>Go to Shopping</label><input type="text" /><button className="edit">Edit</button>
+        <button className="delete">Delete</button></li>
+      </ul>
+
+      <h2>Complete</h2>
+      <ul id="completeTask">
+        <li><input type="checkbox" checked /> <label>Go to Doctor</label><input type="text" /><button className="edit">Edit</button>
+        <button className="delete">Delete</button></li>
+      </ul>
+
+    </div>
+  )
+
+  
+}
+
+
+
+
+
+
+
+
 
 export default Home;
