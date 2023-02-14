09f3b0fa0bf5474633af13b675b1ff90378b695c
ManuelV85 <manuel.villater@gmail.com>
2023-02-14 16:49:18 +0000

    falta configurar boton de borrar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,81 +8,37 @@ import { useEffect, useState } from "react";
 
 
 const Home = ()=>{
-  const [inputValue, setInputValue] = useState("")
+  const[inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-
-  return (
-
+  return(
     <div id="principal" className="Container">
-      <h1> TODO LIST</h1>
+      <h1>TODOS LIST</h1>
       <ul>
-        <li> 
-          <input type="text" 
-          placeholder="Add New Task..."
-          onChange={(e)=> setInputValue(e.target.value)}
-          value={inputValue}></input>
+        <li>
+          <input type="text" onChange={(e) => setInputValue(e.target.value) }
+          value={inputValue}
+          onKeyDown = {(e) =>{
+            if (e.key === 'Enter'){
+              setTodos(todos.concat(inputValue))
+            }}
+          } 
+            placeholder= "Add New Task..."/>
         </li>
-        <li> Go to the doctor <i class="fa-regular fa-trash-can"></i></li>
-        <li>go to shopping <i class="fa-regular fa-trash-can"></i></li>
+        {todos.map((t) => (
+          <li>
+            {t} <i class="fa-regular fa-trash-can"></i>
+          </li>
+        ))}
       </ul>
-      <div>45 task</div>
-    </div>
-  );
-};
-
-
-
-
-
-
-/*
-const Home = () => {
-  const [task, setTask] = useState([])
-  const newTask = (e) =>{
-    if (e.key === 'Enter'){
-      setTask(prev =>[...prev, e.target.value])
-      
-    }
-
-  }
-
-  return (
-    <div className="container">
-      <h1>TODO LIST</h1>
-
-      <h3>Add New Task</h3>
-
-      <br />
-      <ul>
-      <li>
-      <input id="newTask" type="text" placeholder="Add New Task..." onKeyDown={e=>newTask(e)}/>
-      {task.map((task) =>{return <h3>{task}</h3>})}
-      </li>
-
-      <h2>TODO</h2>
-      </ul>
-      <ul id="incompleteTask">
-        <li><input type="checkbox" /><label>Pay Bills</label><input type="text" /><button className="edit">Edit</button>
-        <button className="delete">Delete</button></li>
-        <li><input type="checkbox" /><label>Go to Shopping</label><input type="text" /><button className="edit">Edit</button>
-        <button className="delete">Delete</button></li>
-      </ul>
-
-      <h2>Complete</h2>
-      <ul id="completedTask">
-        <li><input type="checkbox" checked /> <label>Go to Doctor</label><input type="text" /><button className="edit">Edit</button>
-        <button className="delete">Delete</button></li>
-      </ul>
-
+      <div>37 task</div>
     </div>
   )
 
-  
 }
 
-*/
-
-
+  
+    
 
 
 
